bd9453d86a169769ea369321d4611bdb6712a3d9
GIT-SIK <xeno_s@naver.com>
2022-08-31 16:03:58 +0900

    Common - 성격 검사 테스트

--- t_common/Solution.java
+++ t_common/Solution.java
@@ -1,7 +1,5 @@
 package t_common;
 
-import javax.sql.rowset.serial.SerialArray;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +9,22 @@ class Solution {
     public static void main(String[] args) {
 
     Solution s = new Solution();
+
+    /* 입력 타입 */
     String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
     int[] choices = {5, 3, 2, 7, 5};
+
+    String[] survey2 = {"TR", "RT", "TR"};
+    int[] choices2 = {7, 1, 3};
+
+
     /* 입력 */
-    String x = s.solution(survey, choices);
+    String x = s.solution(survey2, choices2);
 
-    }   
-
+    System.out.println(x);
+    }
+    // 1 2 3 4 5 6 7
+    // 3 2 1 0 1 2 3
 
     /* 출력 */
     public String solution(String[] survey, int[] choices) {
@@ -25,7 +32,7 @@ class Solution {
        
         Map<Character,Integer> score = new HashMap();
 
-        score.put('R',0);
+        score.put('R', 0);
         score.put('T', 0);
         score.put('C', 0);
         score.put('F', 0);
@@ -33,7 +40,7 @@ class Solution {
         score.put('M', 0);
         score.put('A', 0);
         score.put('N', 0);
-       
+
         // 1 2 3 4 5 6 7
         // 3 2 1 0 1 2 3
         for(int i=0; i<survey.length; i++){
@@ -53,15 +60,28 @@ class Solution {
 
 
         }
+
         for(char key: score.keySet()){
             int value = score.get(key);
             System.out.println(key+","+value) ;
         }
-        
 
         String answer = "";
+
+        answer += typeEqulse(score,'R', 'T');
+        answer += typeEqulse(score,'C', 'F');
+        answer += typeEqulse(score,'J', 'M');
+        answer += typeEqulse(score,'A', 'N');
+        
         return answer;
     }
 
+    public String typeEqulse(Map<Character,Integer> score ,char Type1, char Type2){
 
+        if(score.get(Type1) >= score.get(Type2)){
+            return String.valueOf(Type1);
+        } else {
+            return String.valueOf(Type2);
+        }
+    }
 }

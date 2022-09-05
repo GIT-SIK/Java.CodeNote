10a45cd885653018b5cce00d78d9e596959bc994
GIT-SIK <xeno_s@naver.com>
2022-09-05 17:41:36 +0900

    CT2 - 두 개 뽑아서 더하기

---
+++ CT2/T3.java
@@ -0,0 +1,44 @@
+/* 문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/68644  */
+/* 두 개 뽑아서 더하기 */
+
+package CT2;
+
+import java.util.*;
+
+public class T3 {
+
+    public static void main(String[] args) {
+
+        T3 t3 = new T3();
+        int[] numbers = {1,2,3,4,3,9};
+        for(int print:t3.sum(numbers)){
+            System.out.print(print + " ");
+        }
+
+    }
+
+        public int[] sum(int[] numbers) {
+
+            Set<Integer> setTemp = new HashSet<>();
+
+            for(int i = 0; i < numbers.length; i++) {
+                if(numbers.length <= i){
+                    break;
+                }
+                for(int j = i+1; j < numbers.length; j++) {
+                    setTemp.add(numbers[i] + numbers[j]);
+                }
+
+            }
+
+            int[] answer = new int[setTemp.size()];
+            Iterator<Integer> iSetTemp = setTemp.iterator();
+            int count = 0;
+                while(iSetTemp.hasNext()) {
+                    answer[count++] = iSetTemp.next();
+                }
+
+            return answer;
+        }
+
+}

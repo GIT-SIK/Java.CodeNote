42a22be8e5b579aaf18435811f4915dd7ec1cdb6
GIT-SIK <xeno_s@naver.com>
2022-10-10 18:04:00 +0900

    DFS 예제

---
+++ Tester/DFS.java
@@ -0,0 +1,31 @@
+package Tester;
+
+import java.util.Arrays;
+
+public class DFS {
+    static boolean[] visited = new boolean[9];
+    static int[][] graph = {{}, {2, 3, 8}, {1, 6, 8}, {1, 5}, {5, 7}, {3, 4, 7}, {2}, {4, 5}, {1, 2}};
+    /*
+
+    2      3      8
+
+
+     */
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int x) {
+        visited[x] = true;
+        sb.append(x).append(" ");
+        for (int i = 0; i < graph[x].length; i++) {
+            if (!visited[graph[x][i]]){
+                System.out.println(Arrays.toString(graph[x]));
+                dfs(graph[x][i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        dfs(1);
+        System.out.println(sb);
+    }
+}

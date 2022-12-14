32993a26f169393d06c0cba9bed6f83f4fe05ef7
GIT-SIK <xeno_s@naver.com>
2022-12-14 23:48:38 +0900

    ETC - IEEE754

--- etc/E/TEST_IEEE754.java
+++ etc/E/TEST_IEEE754.java
@@ -12,6 +12,11 @@ public class TEST_IEEE754 {
         double dubt1 = (double) f1;
         double dub2 = 1.12345671;
 
+        /* 부동소수처리에서 형 변환시 값이 정확하지 않음 */
+        /* double -> float 문제없음 */
+        /* float -> double 문제발생 */
+        /* 1.1234568 -> 1.1234560012817... */
+
         System.out.println(ft1);
         System.out.println(dubt1);
 

91a64d95f0097f8e76420a448898a528dfa052f0
GIT-SIK <xeno_s@naver.com>
2024-01-27 23:34:09 +0900

    stream

--- Code/Stream/TSpace1.java
+++ Code/Stream/TSpace1.java
@@ -24,22 +24,49 @@ public class TSpace1 {
 
 
         /* Method */
-         tConcat(lsStr1, lsStr2);
-         tListSum(lsInt1);
+        tListConcat(lsStr1, lsStr2);
+        tListSum(lsInt1);
+        tListFilter(lsInt1);
 
     }
 
     /* concat */
-    static void tConcat(List<String> ls1, List<String> ls2){
-        Stream.concat(ls1.stream(), ls2.stream()).map(String::toUpperCase).forEach(System.out::println);
+    static void tListConcat(List<String> ls1, List<String> ls2){
+        /* 두 Stream을 합쳐서 대문자로 변환 후 출력합니다. */
+        System.out.print("Concat : ");
+        Stream.concat(ls1.stream(), ls2.stream()).map(String::toUpperCase).forEach(i -> System.out.print(i+ " "));
+        ln();
+
     }
 
-    static void tListSum(List<Integer> ls1){
+    static void tListSum(List<Integer> ls){
         /* reduce (Sum) -> ifPresent */
+        /* 요소들을 람다식으로 두 인자를 받아 하나의 결과를 만들고 ifPresent 결과 여부를 확인 후 결과를 출력합니다.*/
         System.out.print("reduce O : ");
-        ls1.stream().reduce((i, j) -> i + j).ifPresent(temp -> System.out.println(temp));
+        ls.stream().reduce((i, j) -> i + j).ifPresent(temp -> System.out.println(temp));
+
         /* mapToInt -> Sum */
-        System.out.println("reduce X : " + ls1.stream().mapToInt(Integer::intValue).sum());
+        /* Int 변환하여 Sum 메서드를 통해 결과를 출력합니다. */
+        System.out.print("reduce X : " + ls.stream().mapToInt(Integer::intValue).sum());
+        ln();
+
     }
 
+    /* Filter */
+    static void tListFilter(List<Integer> ls) {
+        /* filter -> forEach */
+        System.out.print("filter [item%3==0] : ");
+        ls.stream().filter(item -> item%3==0).forEach(i -> System.out.print(i+ " "));
+        ln();
+    }
+
+
+
+
+
+
+    /* ** println **  */
+    static void ln(){
+        System.out.println();
+    }
 }

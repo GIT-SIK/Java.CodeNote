d903d7831ad5e5f6f078c52030b2769756aa29a0
GIT-SIK <xeno_s@naver.com>
2024-02-03 22:24:01 +0900

    feat(stream) :  add code FileIO (class)

---
+++ example/stream/code/TFile1.java
@@ -0,0 +1,66 @@
+package example.stream.code;
+
+import example.stream.util.TimeUtil;
+
+import java.io.IOException;
+
+/* fileIOBufferedReader */
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/* fileIOStream */
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/* getDirAddress */
+import java.nio.file.Paths;
+
+public class TFile1 {
+    public static void main(String[] args) {
+        TFile1 cFile = new TFile1();
+        cFile.getDirAddress();
+
+        /* Method Execution */
+        TimeUtil.methodExecutionTimer(() -> cFile.fileIOBufferedReader(), "fileIOBufferedReader",true);
+        TimeUtil.methodExecutionTimer(() -> cFile.fileIOStream(), "fileIOStream",true);
+
+        /*
+        *
+        * fileIOBufferedReader : 0.001 ~ 0.007s / avg : 0.003s / Execution Count : 17
+        * fileIOStream : 0.005 ~ 0.016s / avg : 0.012s / Execution Count : 17
+        *
+        * */
+    }
+
+    public void fileIOBufferedReader(){
+        try (BufferedReader reader = new BufferedReader(new FileReader("example/stream/example.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    System.out.println(word);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fileIOStream() {
+        Path filePath = Path.of("example/stream/example.txt");
+        try (
+                Stream<String> lines = Files.lines(filePath)) {
+            lines.flatMap(line -> Stream.of(line.split("\\s+")))
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* 디렉터리 주소을 위한 메소드 */
+    public void getDirAddress(){
+        Path currentDirectory = Paths.get("");
+        System.out.println("Current Directory: " + currentDirectory.toAbsolutePath());
+    }
+}

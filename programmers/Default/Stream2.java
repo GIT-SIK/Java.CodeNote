dc655e274877c9dfbd0ba26199aea67aa1d5698c
GIT-SIK <xeno_s@naver.com>
2024-02-08 04:38:04 +0900

    docs(programmers(Stream2)) :  map과 mapToObj 에 대한 설명 추가 및 코드 수정

--- programmers/Default/Stream2.java
+++ programmers/Default/Stream2.java
@@ -18,7 +18,7 @@ public class Stream2 {
 
     public int[] solution(long n) {
         return new StringBuilder(n+"").reverse().chars()
-                .map(c->c-48)
+                .map(c->(char) c-'0')
                 .toArray();
         /*
         * n+""으로 형 변환 후 reverse()를 통해 뒤집는다.
@@ -26,12 +26,18 @@ public class Stream2 {
         * map으로 각 문자별로 돌며 ASCII '0' 에 해당되는 -48을 뺀다.
          */
 
-
         /* 위 코드와 동일한 동작을 함. */
 //        return new StringBuilder(n+"").reverse().chars()
 //                .mapToObj(c->(char) c-'0')
 //                .mapToInt(Integer::intValue)
 //                .toArray();
 
+        /*
+        * .map(c->(char) c-'0') 와 .mapToObj(c->(char) c-'0') 차이
+        * map은 기본타입 -> 기본타입, 객체타입 -> 객체타입이고
+        * mapToObj는 기본타입 -> 객체타입이다.
+        * 그래서 map은 char이고 mapToObj은 Character 객체로 된다.
+        * 그러므로 mapToInt를 통해 int로 변환하기 때문에 32줄(.mapToInt(Integer::intValue))이 추가가 된다.
+         */
     }
 }

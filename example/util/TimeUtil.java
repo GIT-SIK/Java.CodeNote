ae5eb9068be734b85b3fa8aa5bd253bb38a9b5b9
GIT-SIK <xeno_s@naver.com>
2024-02-10 01:50:21 +0900

    feat(TimeUtil) : add code "method 반환처리"
    - Method에서 반환되는 모든 값을 대응하기 위한 소스 추가
    - void 타입 메소드는 기존 메소드 사용.

--- example/util/TimeUtil.java
+++ example/util/TimeUtil.java
@@ -1,7 +1,25 @@
 package example.util;
 
+import java.util.concurrent.Callable;
+
 public class TimeUtil {
     /* 실행 시간 측정 메소드 */
+    public static <T> T methodExecutionTimer(Callable<T> method, String methodName, boolean allowsPrint) {
+        long start = System.currentTimeMillis();
+        T result = null;
+        try {
+            result = method.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        if(allowsPrint) {
+            System.out.println();
+        }
+        System.out.println(methodName + "의 실행 시간: " + (end - start) / 1000.0 + "초 \n");
+        return result;
+    }
+
     public static void methodExecutionTimer(Runnable method, String methodName, boolean allowsPrint) {
         long start = System.currentTimeMillis();
         method.run();
@@ -11,4 +29,5 @@ public class TimeUtil {
         }
         System.out.println(methodName + "의 실행 시간: " + (end - start) / 1000.0 + "초 \n");
     }
+
 }

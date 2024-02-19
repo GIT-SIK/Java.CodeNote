f9417e46ffb7b02734cd7298388056e2ce2f771a
GIT-SIK <xeno_s@naver.com>
2024-02-19 19:43:02 +0900

    feat(programmers(Stream9)) : add code (배열내 조건에 맞는값 개수 세기)

---
+++ programmers/Default/Stream9.java
@@ -0,0 +1,27 @@
+package Programmers.Default;
+
+import java.util.Arrays;
+
+public class Stream9 {
+    public static void main(String[] args) {
+        Stream9 st9 = new Stream9();
+        System.out.println(st9.solution(new int[]{1,177,787}));
+
+    }
+    public int solution(int[] array) {
+        /* 문제 ( 단, Stream을 활용하여 풀 것.)
+        * 배열에 있는 7의 개수 출력
+         */
+        int[] c = {0};
+        Arrays.stream(array)
+                .mapToObj(i -> Integer.toString(i).split(""))
+                .forEach(r -> { // 각 인덱스별로 분할된 값 돌면서 비교함. 1 -> 1 7 7 -> 7 8 7
+                    for(String str : r){
+                        if(str.equals("7")) {
+                            c[0]++;
+                        }
+                    };
+                });
+        return c[0];
+    }
+}

a32acea1d1378b088a67d8d3d0b34af802bf5dd4
GIT-SIK <xeno_s@naver.com>
2024-03-18 22:00:35 +0900

    feat(stream - TCode3) : add code 이중 stream (interface, class)
    * 이중 for문 -> Stream으로 대체하기
      - 다양한 방식 추가
      - 응용 추가

--- example/stream/code/TCode3.java
+++ example/stream/code/TCode3.java
@@ -13,9 +13,15 @@ public class TCode3 implements TCode3Operations {
 
         /* Method Execution */
         TimeUtil.methodExecutionTimer(() -> tc3.tNestedIntStream(),"tNestedIntStream", true);
+        TimeUtil.methodExecutionTimer(() -> tc3.tNestedIntStream2(),"tNestedIntStream2", true);
+        TimeUtil.methodExecutionTimer(() -> tc3.tNestedIntStream3(),"tNestedIntStream3", true);
+
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {3, 4, 5, 6, 7};
+        TimeUtil.methodExecutionTimer(() -> tc3.tNestedIntStream4(arr1, arr2),"t+T+NestedIntStream4", true);
     }
 
-    /* [] Nested IntStream */
+    /* [](mapToObj -> Boxed) Nested IntStream */
     public void tNestedIntStream() {
         IntStream.rangeClosed(1, 9)
                 .mapToObj(i -> IntStream.rangeClosed(1, 9)
@@ -25,5 +31,35 @@ public class TCode3 implements TCode3Operations {
                 .forEach(sum ->
                     System.out.println(Arrays.toString(sum)));
     }
+
+    /* [](flatMap) Nested IntStream */
+    public void tNestedIntStream2() {
+        IntStream.rangeClosed(1, 9)
+                .flatMap(i -> IntStream.rangeClosed(1, 9)
+                        .map(j -> i + j))
+                .forEach(System.out::println);
+    }
+
+    /* [](forEach) Nested IntStream */
+    public void tNestedIntStream3() {
+        IntStream.rangeClosed(1, 9)
+                .forEach(i -> {
+                    IntStream.rangeClosed(1, 9)
+                            .map(j -> i + j)
+                            .forEach(System.out::println);
+                });
+    }
+
+    /* [](forEach+arr) Nested IntStream */
+    public void tNestedIntStream4(int[] arr, int[] arr2) {
+        IntStream.range(0, arr.length)
+                .forEach(i -> {
+                    System.out.println();
+                    IntStream.range(0, arr2.length)
+                            .map(j -> arr[i] + arr2[j])
+                            .forEach(r -> System.out.print(r + " "));
+                });
+    }
+
 }
 
--- example/stream/interfaces/TCode3Operations.java
+++ example/stream/interfaces/TCode3Operations.java
@@ -2,6 +2,15 @@ package example.stream.interfaces;
 
 public interface TCode3Operations {
 
-    /* [] Nested IntStream */
+    /* [](mapToObj -> Boxed) Nested IntStream */
     void tNestedIntStream();
+
+    /* [](flatMap) Nested IntStream */
+    void tNestedIntStream2();
+
+    /* [](forEach) Nested IntStream */
+    void tNestedIntStream3();
+
+    /* [](forEach+arr) Nested IntStream */
+    void tNestedIntStream4(int[] arr, int[] arr2);
 }

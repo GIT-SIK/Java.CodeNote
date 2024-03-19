c23663720a4eacd638be64cbdb2e1ebac7a29e65
GIT-SIK <xeno_s@naver.com>
2024-03-20 08:32:48 +0900

    feat(백준/etc) :  add code (Input File)
    - 백준 코딩테스트 입력별 데이터 가공 예제 (스트림 방식)

---
+++ 백준/etc/input.java
@@ -0,0 +1,40 @@
+package 백준.etc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class input {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("백준/etc/input.txt"));
+
+        /* (스트림 형태) Input 구조 별 데이터 가공*/
+        /* 구조 (2 가져오기)
+         *   1 2
+         */
+        int val = Integer.parseInt(br.readLine().split(" ")[1]);
+
+
+        /* 구조
+         *   1 2 3 4 5 6
+        */
+        String[] lsStr = br.readLine().split(" ");
+        int[] ls = Arrays.stream(lsStr).mapToInt(Integer::parseInt).toArray();
+
+
+        /* 구조
+         *   1 2
+         *   3 4
+         */
+        int[][] ls2 = br.lines().takeWhile(l -> !l.isEmpty()).map(l -> Arrays.stream(l.split(" ")).mapToInt(Integer::parseInt).toArray()).toArray(int[][] :: new);
+
+
+         /* 구조
+         *   1
+         *   2
+         */
+        int[] ls3 = br.lines().takeWhile(l -> !l.isEmpty()).mapToInt(Integer::parseInt).toArray();
+    }
+
+}

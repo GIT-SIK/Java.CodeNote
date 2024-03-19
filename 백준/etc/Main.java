423ae26ef8ecd948b076e0413fd5f06b70920ba7
GIT-SIK <xeno_s@naver.com>
2024-03-19 10:54:58 +0900

    feat(백준/etc) :  add code (Test File)

---
+++ 백준/etc/Main.java
@@ -0,0 +1,18 @@
+package 백준.etc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new FileReader("백준/etc/input.txt"));
+            /* ********************************************** */
+
+
+
+
+            /* ************************************************ */
+            br.close();
+    }
+}

a8ddf5d63805433911314c130f6ac30c3d263b86
GIT-SIK <xeno_s@naver.com>
2024-05-11 23:09:39 +0900

    feat(input.java) : 일부 코드 제거
    - takeWhile 제거 / 동작에 큰 문제 없음.

--- 백준/etc/input.java
+++ 백준/etc/input.java
@@ -9,32 +9,34 @@ public class input {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("백준/etc/input.txt"));
 
-        /* (스트림 형태) Input 구조 별 데이터 가공*/
-        /* 구조 (2 가져오기)
-         *   1 2
+        /* (스트림 형태) Input 구조 별 데이터 가공 */
+        /*
+         * 구조 (2 가져오기)
+         * 1 2
          */
         int val = Integer.parseInt(br.readLine().split(" ")[1]);
 
-
-        /* 구조
-         *   1 2 3 4 5 6
-        */
+        /*
+         * 구조
+         * 1 2 3 4 5 6
+         */
         String[] lsStr = br.readLine().split(" ");
         int[] ls = Arrays.stream(lsStr).mapToInt(Integer::parseInt).toArray();
 
-
-        /* 구조
-         *   1 2
-         *   3 4
+        /*
+         * 구조
+         * 1 2
+         * 3 4
          */
-        int[][] ls2 = br.lines().takeWhile(l -> !l.isEmpty()).map(l -> Arrays.stream(l.split(" ")).mapToInt(Integer::parseInt).toArray()).toArray(int[][] :: new);
+        int[][] ls2 = br.lines().map(l -> Arrays.stream(l.split(" ")).mapToInt(Integer::parseInt).toArray())
+                .toArray(int[][]::new);
 
-
-         /* 구조
-         *   1
-         *   2
+        /*
+         * 구조
+         * 1
+         * 2
          */
-        int[] ls3 = br.lines().takeWhile(l -> !l.isEmpty()).mapToInt(Integer::parseInt).toArray();
+        int[] ls3 = br.lines().mapToInt(Integer::parseInt).toArray();
     }
 
 }

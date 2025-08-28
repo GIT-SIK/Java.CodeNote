d2c86260a541dd20d1c67843eb3b89415a1aff00
SIK <96171462+GIT-SIK@users.noreply.github.com>
2025-08-28 12:01:17 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 228 ms, Memory: 20576 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Matcher matcher = Pattern.compile("<[^>]+>|\\s+|[^<\\s]+").matcher(br.readLine());
+        List<String> divStr = new ArrayList<>(); 
+        while (matcher.find()) {
+            String mgs = matcher.group(0);
+            StringBuffer sb = new StringBuffer(mgs);
+                if(!mgs.startsWith("<")) {
+                    sb.reverse();
+                }
+            divStr.add(sb.toString());
+        }
+        for(String ds : divStr) {
+            bw.write(ds);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
